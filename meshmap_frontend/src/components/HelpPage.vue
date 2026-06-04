447cacc4f6ebe5a3a128a7a8f9439928794465be
吴文峰 <kevin@lmve.net>
2026-06-04 16:07:10 +0800

    修改帮助页面

--- meshmap_frontend/src/components/HelpPage.vue
+++ meshmap_frontend/src/components/HelpPage.vue
@@ -15,6 +15,8 @@
           <ul>
             <li>默认地址：<strong>mesh.gat-iot.com</strong></li>
             <li>默认端口：<strong>1883</strong></li>
+            <li>用户名称：<strong>meshdev</strong></li>
+            <li>密码：<strong>large4cats</strong></li>
           </ul>
         </section>
 
@@ -26,6 +28,11 @@
             <li>使用 Meshtastic 默认 PSK：<strong>AQ==</strong>。</li>
           </ul>
           <p>如果使用自定义加密密钥，数据可能会被判定为无法解密并丢弃。</p>
+        </section>
+
+        <section>
+          <h3>反馈问题</h3>
+          <p>如果遇到 bug，请在 GitHub <a href="https://github.com/wuwenfengmi1998/meshtastic_mqtt_server" target="_blank" rel="noopener noreferrer">提交 issue</a>，或联系邮箱 <a href="mailto:kevin@lmve.net">kevin@lmve.net</a>。</p>
         </section>
 
       </div>
